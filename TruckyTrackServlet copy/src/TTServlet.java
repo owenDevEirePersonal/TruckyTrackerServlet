a9e83a046d784649acece8a6a6a9e5dba28f4f20
owenDevEirePersonal <owen@deveire.com>
2017-05-10 11:57:05 +0100

    <Implemented Store Keg>
    Added code that implements the servlet-side of keg tracking. The
    servlet now takes in the user id, the id of a scanned keg, the user’s
    lat and the user’s Lon.
    
    The servlet then uses this info to check if the user id and the keg id
    exist in the ids and KegKeys tables respectively. Then if not already
    on the table, it adds the keg and driver to the table KegDrivers which
    tracks which driver has which kegs. It also adds the current time and
    entered lat Lon as the pickup location and time to a new entry on the
    KegHistory table.
    
    If the keg is already on KegDrivers then it is removed, and the current
    time and entered lat Lon are entered into an existing entry on
    KegHistory as the dropoff time and location.
    
    +Added storeKeg().
    #Modified doGet to accept request=storekeg.

--- TruckyTrackServlet copy/src/TTServlet.java
+++ TruckyTrackServlet copy/src/TTServlet.java
@@ -47,6 +47,18 @@ public class TTServlet extends HttpServlet
 		{
 		switch(request.getParameter("request").toLowerCase())
 		{
+			case "storekeg": 
+				if(request.getParameter("lat") != null && request.getParameter("lon") != null && request.getParameter("id") != null && request.getParameter("kegid") != null)
+				{
+					Double inLat = Double.parseDouble(request.getParameter("lat"));
+					Double inLong = Double.parseDouble(request.getParameter("lon"));
+					int id = Integer.parseInt(request.getParameter("id"));
+					String inkegID = request.getParameter("kegid");
+
+					storeKeg(id, inkegID, inLat, inLong);
+				}
+			break;
+			
 			case "storelocation": 
 				if(request.getParameter("lat") != null && request.getParameter("lon") != null && request.getParameter("id") != null)
 				{
@@ -201,6 +213,175 @@ public class TTServlet extends HttpServlet
 	         //out.print("insert is " + sql);
 	         
 	         //out.println("Insert complete.");
+
+	         // Clean-up environment
+	         rs.close();
+	         stmt.close();
+	         conn.close();
+	    }
+	    catch(SQLException se)
+	    {
+	         //Handle errors for JDBC
+	         se.printStackTrace();
+	    }
+	    catch(Exception e)
+	    {
+	         //Handle errors for Class.forName
+	         e.printStackTrace();
+	    }
+	    finally
+	    {
+	         //finally block used to close resources
+	         try
+	         {
+	            if(stmt!=null){stmt.close();};
+	         }
+	         catch(SQLException se2)
+	         {
+	         }// nothing we can do
+	         try
+	         {
+	            if(conn!=null){conn.close();}
+	         }
+	         catch(SQLException se)
+	         {
+	            se.printStackTrace();
+	         }//end finally try
+	     } //end try
+	}
+	
+	private void storeKeg(int inID, String inKegID, double inLat, double inLon)
+	{
+		// JDBC driver name and database URL
+	    final String JDBC_DRIVER="com.mysql.jdbc.Driver";  
+	    final String DB_URL="jdbc:mysql://localhost/truckytrackdatabase";
+	    
+	    //  Database credentials
+	    final String USER = "user";
+	    final String PASS = "";
+	    Statement stmt = null;
+	    Connection conn = null;
+	    
+	    int count = 0;
+	    boolean kegIsFull = true;
+	    
+	    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        java.util.Date currentTimeStamp = new java.util.Date();
+	    
+	    try
+	    {
+	         // Register JDBC driver
+	         Class.forName("com.mysql.jdbc.Driver");
+
+	         // Open a connection
+	         conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+	         // Execute SQL query
+	         stmt = conn.createStatement();
+	         String sql;
+	         sql = "SELECT DISTINCT id FROM ids;";
+	         ResultSet rs = stmt.executeQuery(sql);
+	         
+	         ArrayList<Integer> returnIds = new ArrayList<Integer>();
+	         boolean idExists = false;
+	         
+
+
+	         // Extract data from result set
+	         while(rs.next())
+	         {
+	        	 count++;
+	            //Retrieve by column name
+	        	 //out.print("checking id " + rs.getInt("id"));
+	        	 int currentID = rs.getInt("id");
+	        	 //out.println("  &: id " + currentID + " is a to " + inID);
+	        	 if(currentID == inID)
+	        	 {
+	        		 //out.println("id " + currentID + " is equal to " + inID);
+	        		 idExists = true;
+	        		 break;
+	        	 }
+	         }
+	         
+	         if(idExists)
+	         {
+	        	 //Execute SQL query
+	        	 //[Get full and empty ids from KegKeys]
+	        	 String currentFullID;
+	        	 String currentEmptyID;
+	         	sql = "SELECT DISTINCT fullID, emptyID FROM KegKeys WHERE fullID='" + inKegID + "' OR emptyID='" + inKegID + "';";
+	         	//out.println(sql);
+	         	rs = stmt.executeQuery(sql);
+	         
+	         	ArrayList<Integer> returnKegIds = new ArrayList<Integer>();
+	         	boolean kegIdExists = false;
+
+	         	// Extract data from result set
+	         	while(rs.next())
+	         	{
+	         		//Retrieve by column name
+	         		//out.print("checking id " + rs.getInt("id"));
+	         		currentFullID = rs.getString("fullID");
+	         		currentEmptyID = rs.getString("emptyID");
+	         		//out.println("  &: id " + currentID + " is a to " + inID);
+	         		kegIdExists = true;
+	         		if(currentFullID.matches(inKegID))
+	         		{
+	         			//out.println("id " + currentID + " is equal to " + inID);
+	         			kegIsFull = true;
+	         			break;
+	         		}
+	         		else if(currentEmptyID.matches(inKegID))
+	         		{
+	         			kegIsFull = false;
+	         			break;
+	         		}	
+	         	}
+	         	//[/Get full and empty ids from KegKeys]
+	         
+	         	//Execute SQL query
+	         	//[Determine whether or no keg has been pickedUp or not] 
+	         	boolean kegIsInKegDrivers = false;
+	         	sql = "SELECT DISTINCT kegID FROM KegDrivers WHERE (kegID = '" + inKegID + "');";
+	         	rs = stmt.executeQuery(sql);
+	         	
+	         	if(rs.next())
+	         	{
+	         		kegIsInKegDrivers = true;
+	         	}	
+	         	//[/Determine whether or no keg has been pickedUp or not]
+	         
+	         	//[Update History Table and the table of keg currently being transported by drivers]
+	         	if(idExists && kegIdExists)
+	         	{
+	         		if(kegIsInKegDrivers)
+	         		{
+	         			//keg is being dropped off and is therefore removed from KegDrivers.
+	         			sql = "DELETE FROM KegDrivers WHERE (kegID = '" + inKegID + "');";
+	         			//out.println(sql);
+	         			stmt.executeUpdate(sql);
+	         		
+	         			sql = "UPDATE KegHistory SET DroppedAtLat=" + inLat + ", DroppedAtLon=" + inLon + ", DroppedAtTime='" + dateFormat.format(currentTimeStamp) + "' WHERE (id = '" + inKegID + "' AND DroppedAtTime IS NULL);";
+	         			//out.println(sql);
+	         			stmt.executeUpdate(sql);
+	         		}
+	         		else
+	         		{
+	         			//keg is being picked up and is therefore added to Kegdrivers.
+	         			sql = "INSERT INTO KegDrivers (DriverID, KegID) VALUES (" + inID + ", '" + inKegID + "');";
+	         			//out.println(sql);
+	         			stmt.executeUpdate(sql);
+	         			
+	         			sql = "INSERT INTO KegHistory (id, DriverID, PickedUpAtLat, PickedUpAtLon, PickedUpTime) VALUES ('" + inKegID + "', " + inID + ", " + inLat + ", " + inLon + ", '" + dateFormat.format(currentTimeStamp) + "');";
+	         			//out.println(sql);
+	         			stmt.executeUpdate(sql);
+	         		}
+
+	        	 
+	         }
+	         //[Update History Table and the table of keg currently being transported by drivers]
+	         }
+	         
 
 	         // Clean-up environment
 	         rs.close();
